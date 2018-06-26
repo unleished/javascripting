05eae36420aa2f450d7175211b34cf54e9b3ba8b
Leisha Magsisi <leisha.gallagher@gmail.com>
2018-06-26 00:52:28 +0000

    Initial Commit for Javascripting

---
+++ if-statements.js
@@ -0,0 +1,8 @@
+var fruit = 'orange';
+
+if (fruit.toString() > 5) {
+  console.log('The fruit name has five characters or less.');
+} else {
+  console.log('The fruit name has more than five characters.');
+}
+
---
+++ rounding-numbers.js
@@ -0,0 +1,6 @@
+var roundup = 1.5;
+Math.round(1.5);
+
+var rounded = Math.round(roundup);
+console.log(rounded);
+
